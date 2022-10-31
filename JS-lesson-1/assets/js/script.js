7f4a22a57e1c33b56fd058beaef7f1c91c98391e
Vachek-hooves <vachek0602@gmail.com>
2022-10-31 22:19:29 +0200

    js lesson 1 completed

--- JS-lesson-1/assets/js/script.js
+++ JS-lesson-1/assets/js/script.js
@@ -8,10 +8,34 @@
 /*more then one 
   line comment*/
 
+alert = alert('Hello! Lets start)');
+
 let myName = "Slava";
     my_Age = 34;
     fromCity = 'Kyiv';
-    myGender = 'Male';
+    myOfGender = 'Male';
     $Status = 'marriage';
-console.log(Name,Age,fromCity,Gender,Status)
+console.log(myName,my_Age,fromCity,myOfGender,$Status);
 
+let yourName = prompt ('enter your name');
+console.log(yourName);
+
+const presentYear = 2022;
+let yourBirthYear = prompt('Enter your year of birht');
+console.log(presentYear - yourBirthYear, 'Age');
+
+let squareSide = prompt ('Do you mind to practice match a little? Put square side length and i will tell what perimeter wiil be in result))' )
+const squareFormula = 4;
+console.log(squareFormula*squareSide, 'its your square))')
+
+let circleArea = prompt  ('Also i can find circle area, so enter its radius & il find circle S)');
+const circleP = 3.14;
+console.log(circleP*circleArea**2,'circle S');
+
+let roadDistance = prompt ('I can find how fast you should drive to come on time.First enter below distance in "km" between your start & finish.');
+let roadTime = prompt ('Now, enter your voyage time in hrs');
+console.log(roadDistance/roadTime,"km/hr. Remember-safety first");
+
+let $dollar = prompt("Do you want to know how much mony you save in euro? Just put amount in dollar down below.");
+let euro = 1.01;
+console.log($dollar*euro,"amount you will get");
