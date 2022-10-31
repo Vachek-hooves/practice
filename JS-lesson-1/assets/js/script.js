cf30217513e53377469bd55f0faa0a701e58d45f
Vachek-hooves <vachek0602@gmail.com>
2022-10-31 19:52:15 +0200

    js in progress

---
+++ JS-lesson-1/assets/js/script.js
@@ -0,0 +1,17 @@
+/* wrong names*/
+// let 1Name = 'Slava';
+// let my-age = 34;
+// let 123 = 'male';
+// let -my status = 'marriage';
+
+//one line comment
+/*more then one 
+  line comment*/
+
+let myName = "Slava";
+    my_Age = 34;
+    fromCity = 'Kyiv';
+    myGender = 'Male';
+    $Status = 'marriage';
+console.log(Name,Age,fromCity,Gender,Status)
+
