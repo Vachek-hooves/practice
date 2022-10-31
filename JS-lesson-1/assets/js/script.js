6df916b8014fdf75fc88c2e1350e3d08574b0f97
Vachek-hooves <vachek0602@gmail.com>
2022-10-31 22:46:46 +0200

    ok

--- JS-lesson-1/assets/js/script.js
+++ JS-lesson-1/assets/js/script.js
@@ -18,24 +18,24 @@ let myName = "Slava";
 console.log(myName,my_Age,fromCity,myOfGender,$Status);
 
 let yourName = prompt ('enter your name');
-console.log(yourName);
+console.log(yourName,', hello!');
 
 const presentYear = 2022;
-let yourBirthYear = prompt('Enter your year of birht');
+let yourBirthYear = prompt('Enter year of birth');
 console.log(presentYear - yourBirthYear, 'Age');
 
-let squareSide = prompt ('Do you mind to practice match a little? Put square side length and i will tell what perimeter wiil be in result))' )
+let squareSide = prompt ('Do you mind to practice match a little? Put square side length and i will tell what perimeter to be in result))' )
 const squareFormula = 4;
-console.log(squareFormula*squareSide, 'its your square))')
+console.log(squareFormula*squareSide, "it's your square in cm))");
 
-let circleArea = prompt  ('Also i can find circle area, so enter its radius & il find circle S)');
+let circleArea = prompt  ("Also i can find circle area, so enter its radius & I'll find circle S)");
 const circleP = 3.14;
 console.log(circleP*circleArea**2,'circle S');
 
 let roadDistance = prompt ('I can find how fast you should drive to come on time.First enter below distance in "km" between your start & finish.');
-let roadTime = prompt ('Now, enter your voyage time in hrs');
-console.log(roadDistance/roadTime,"km/hr. Remember-safety first");
+let roadTime = prompt ('Now, enter your voyage time in min');
+console.log(roadDistance/(roadTime/60),"km/hr. Remember-safety first!");
 
-let $dollar = prompt("Do you want to know how much mony you save in euro? Just put amount in dollar down below.");
+let $dollar = prompt("Do you want to know how much money you save in euro? Just put amount in dollar down below.");
 let euro = 1.01;
 console.log($dollar*euro,"amount you will get");
